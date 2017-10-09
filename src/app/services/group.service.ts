c9cd2c5a9a89ff5c5baf86fa9146f9c2a3befdc4
flysaiah <mayeis01@yahoo.com>
2017-10-09 18:19:15 -0400

    Simple group page with create/delete group

---
+++ src/app/services/group.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { AuthService } from './auth.service';
+@Injectable()
+export class GroupService {
+
+  options;
+  domain = this.authService.domain;
+
+  createAuthenticationHeaders() {
+    this.authService.loadToken();
+    this.options = new RequestOptions({
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'authorization': this.authService.authToken
+      })
+    });
+  }
+
+  createGroup(groupName: string, username: string) {
+    this.createAuthenticationHeaders();
+    return this.http.post(this.domain + '/api/createGroup', { name: groupName }, this.options).map(res => res.json());
+  }
+
+  getGroupInfo(groupName: string) {
+    this.createAuthenticationHeaders();
+    return this.http.post(this.domain + '/api/getGroupInfo', { name: groupName }, this.options).map(res => res.json());
+  }
+
+  disbandGroup(groupName: string) {
+    this.createAuthenticationHeaders();
+    return this.http.post(this.domain + '/api/disbandGroup', { name: groupName }, this.options).map(res => res.json());
+
+  }
+
+  constructor(
+    private authService: AuthService,
+    private http: Http
+  ) { }
+
+}
