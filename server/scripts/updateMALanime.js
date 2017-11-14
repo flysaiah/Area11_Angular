c76b5a256f09e72cda22fca0d9e9a08b33a06f98
flysaiah <mayeis01@yahoo.com>
2017-11-14 14:55:07 -0500

    Added script for updating MAL anime. Closes #30

--- server/scripts/updateMALanime.js
+++ server/scripts/updateMALanime.js
@@ -17,55 +17,83 @@ Anime.find({}, (err, animeList) => {
     mongoose.disconnect();
     return err;
   } else {
-    for (let anime of animeList) {
-      if (!anime.malID) {
-        continue;
-      }
-      var async = require("async");
-      const query = encodeURIComponent(anime["name"]);
-      console.log(query);
-      request.get({url: 'https://area11-burn:yuibestgirl4ever@myanimelist.net/api/anime/search.xml?q=' + query}, function (err, response, body) {
-        if (!err) {
-          let malList = JSON.parse(parser.toJson(body).toString())["anime"]["entry"];
-          async.eachSeries(malList, function updateAnime (malAnime, done) {
-            let newAnime = new Anime({
-              user: anime['user'],
-              name: anime['name'],
-              description: anime['description'],
-              rating: anime['rating'],
-              thumbnail: anime['thumbnail'],
-              malID: anime['malID'],
-              category: anime['category'],
-              isFinalist: anime['isFinalist'],
-              genres: anime['genres'],
-              startDate: anime['startDate'],
-              endDate: anime['endDate'],
-              type: anime['type'],
-              englishTitle: anime['englishTitle'],
-              status: anime['status']
-            });
-            Anime.update({ malID: malAnime.id }, { $set: {
-              description: (typeof malAnime["synopsis"] == "string" ? malAnime["synopsis"] : "").toString(),
-              rating: (typeof malAnime["score"] == "string" ? malAnime["score"] : "").toString(),
-              thumbnail: (typeof malAnime["image"] == "string" ? malAnime["image"] : "").toString(),
-              startDate: (new Date(malAnime["start_date"])).toLocaleDateString(),
-              endDate: (new Date(malAnime["end_date"])).toLocaleDateString(),
-              type: (typeof malAnime["type"] == "string" ? malAnime["type"] : "").toString(),
-              englishTitle: (typeof malAnime["english"] == "string" ? malAnime["english"] : "").toString(),
-              status: (typeof malAnime["status"] == "string" ? malAnime["status"] : "").toString()
-            } }, done);
-          }, function allDone (err) {
-            if (err) {
-              console.log(err);
-            } else {
-              mongoose.disconnect();
-            }
-          });
-        } else {
-          console.log(err);
+    let animeSet = new Set();
+    let updateMALAnime = function(arr) {
+      if (!arr.length) {
+        console.log("Done")
+        setTimeout(() => {
+          mongoose.disconnect();
           return;
+        }, 10000)
+      } else {
+        let anime = arr[0];
+        if (animeSet.has(anime["name"])) {
+          arr.splice(0,1);
+          updateMALAnime(arr);
+        } else {
+          animeSet.add(anime["name"]);
+          console.log("Current: " + anime["name"])
+          if (!anime.malID) {
+            arr.splice(0,1);
+            updateMALAnime(arr);
+          }
+          setTimeout(() => {
+            const query = encodeURIComponent(anime["name"]);
+            request.get({url: 'https://area11-burn:yuibestgirl4ever@myanimelist.net/api/anime/search.xml?q=' + query}, function (err, response, body) {
+              if (!err) {
+                let entries = JSON.parse(parser.toJson(body).toString())["anime"]["entry"];
+                let malAnime;
+                if (!entries.length && !entries["title"]) {
+                  console.log(entries);
+                  arr.splice(0,1);
+                  updateMALAnime(arr);
+                  return;
+                } else if (!entries.length) {
+                  malAnime = entries;
+                } else {
+                  for (let entry of entries) {
+                    if (entry["title"] == anime["name"]) {
+                      malAnime = entry;
+                      break;
+                    }
+                  }
+                }
+                if (malAnime && malAnime["title"]) {
+                  Anime.update({ malID: malAnime.id }, { $set: {
+                    description: (typeof malAnime["synopsis"] == "string" ? malAnime["synopsis"] : "").toString(),
+                    rating: (typeof malAnime["score"] == "string" ? malAnime["score"] : "").toString(),
+                    thumbnail: (typeof malAnime["image"] == "string" ? malAnime["image"] : "").toString(),
+                    startDate: (new Date(malAnime["start_date"])).toLocaleDateString(),
+                    endDate: (new Date(malAnime["end_date"])).toLocaleDateString(),
+                    type: (typeof malAnime["type"] == "string" ? malAnime["type"] : "").toString(),
+                    englishTitle: (typeof malAnime["english"] == "string" ? malAnime["english"] : "").toString(),
+                    status: (typeof malAnime["status"] == "string" ? malAnime["status"] : "").toString()
+                  } }, { multi: true }, (err) => {
+                    if (err) {
+                      console.log(err);
+                    } else {
+                      arr.splice(0,1);
+                      updateMALAnime(arr);
+                    }
+                  });
+                } else {
+                  console.log("----- LAME ISSUE -----");
+                  console.log(anime["name"]);
+                  console.log(malAnime);
+                  console.log("----- ENDOFISSUE -----");
+                  arr.splice(0,1);
+                  updateMALAnime(arr);
+                }
+              } else {
+                console.log(err);
+                mongoose.disconnect();
+                return;
+              }
+            });
+          }, 3000);
         }
-      });
+      }
     }
+    updateMALAnime(animeList);
   }
 });
