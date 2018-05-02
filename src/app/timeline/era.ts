25661ef0be93e59cde3af8d4e4aa4d3b40eeaa58
flysaiah <mayeis01@yahoo.com>
2018-05-01 19:08:25 -0500

    Added color picker for timelines. Closes #121

--- src/app/timeline/era.ts
+++ src/app/timeline/era.ts
@@ -6,6 +6,7 @@ export class Era {
     public endDate?: string,
     public location?: string,
     public metadata?: string,
-    public subHeader?: string
+    public subHeader?: string,
+    public backgroundColor?: string
   ) {}
 }
